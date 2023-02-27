3235c4a3829d4a67eadb758538d7e9286e8e3205
strykerin <dacosta.pereirafabio@gmail.com>
2023-02-26 18:08:08 -0700

    simplify sending notification code

--- sending-notifs-sdk/index.ts
+++ sending-notifs-sdk/index.ts
@@ -7,23 +7,25 @@ dotenv.config();
 const PK = process.env.CHANNEL_PRIVATE_KEY
 const Pkey = `0x${PK}`;
 const signer = new ethers.Wallet(Pkey);
-const receiver = 'eip155:5:0x9dc230703b28b739c7Fcee6ff20F59fBD9F7CC65'
+const receiver = 'eip155:5:0xA465B5596e59FA87aB806515EB14D7a74873BC33'
 const channel = 'eip155:5:0x98821462cB7Aa6fca72c6590D04bEA098c4483D0'
+const myNotificationTitle = 'You received a new notification :)'
+const myNotificationBody = 'Hello world'
 
 const sendNotification = async () => {
     try {
         const apiResponse = await PushAPI.payloads.sendNotification({
             signer,
-            type: 3, // target
-            identityType: 2, // direct payload
+            type: 3,
+            identityType: 2,
             notification: {
-                title: `[SDK-TEST] notification TITLE:`,
-                body: `[sdk-test] notification BODY`
+                title: myNotificationTitle,
+                body: myNotificationBody
             },
             payload: {
-                title: `[sdk-test] payload title`,
-                body: `sample msg body`,
-                cta: '',
+                title: myNotificationTitle,
+                body: myNotificationBody,
+                cta: 'https://youtube.com',
                 img: ''
             },
             recipients: receiver,
